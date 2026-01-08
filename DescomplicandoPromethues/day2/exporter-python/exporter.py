b2bfd68d9ffbca649683b5d612695c44b032a42e
Gabriel Cristian <gabriel@megaconecta.com.br>
2026-01-08 17:25:48 -0300

    Ajustando nome do diretório'

--- DescomplicandoPromethues/day2/exporter-python/exporter.py
+++ DescomplicandoPromethues/day2/exporter-python/exporter.py
@@ -3,8 +3,20 @@ import time
 from prometheus_client import start_http_server, Gauge
 
 # Definimos a métrica globalmente para evitar reinicializações
-GAUGE_ASTRONAUTS = Gauge('number_astronauts', 'Number of astronauts in space')
 URL_API = "http://api.open-notify.org/astros.json"
+URL_LOCAL_ISS = "http://api.open-notify.org/iss-now.json"
+
+def get_location():
+  """
+  Busca a latitude e longitude da ISS
+  """
+  try:
+      response = requests.get(URL_LOCAL_ISS, timeout=5)
+      data = response.json()
+      return data['iss_position']
+  except Exception as e:
+        print(f"Erro ao buscar localização da ISS: {e}")
+        raise e
 
 def get_number_astronauts():
     """
@@ -21,17 +33,25 @@ def update_metrics():
     Loop infinito que atualiza a métrica sem derrubar o serviço.
     """
     print("Iniciando coleta de dados...")
+
+
+    astronauts = Gauge('number_astronauts', 'Number of astronauts in space')
+    longitude = Gauge('longitude_iss', 'Longitude of the ISS')
+    latitude = Gauge('latitude_iss', 'Latitude of the ISS')
     while True:
         try:
-            # Pegamos o valor uma única vez
-            valor = get_number_astronauts()
-            GAUGE_ASTRONAUTS.set(valor)
-            print(f"Métrica atualizada: {valor} astronautas no espaço.")
+            number_astronauts_now = get_number_astronauts()
+            location_iss = get_location()
+            astronauts.set(number_astronauts_now)
+            longitude.set(float(location_iss['longitude']))
+            latitude.set(float(location_iss['latitude']))
+
+            print(f"Métrica atualizada: {number_astronauts_now} astronautas no espaço.")
+            print(f"Localização da ISS - Latitude: {location_iss['latitude']}, Longitude: {location_iss['longitude']}")
         except Exception as e:
-            # Logamos o erro, mas o loop continua vivo
             print(f"Erro ao coletar dados: {e}. Tentando novamente em 10s...")
         
-        time.sleep(10)
+        time.sleep(20)
 
 def init_exporter():
     try:
